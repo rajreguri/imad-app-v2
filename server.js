0bcab91d202b68a26f9c4c48728dd2cbeadfd84b
rajreguri <rajreguri1@gmail.com>
2017-02-08 13:28:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'calender.html'));
 });
 
-app.get('/',function (req,res) {
-    res.sendfile(path.join(__dirname, 'ui', 'about.html'));
-});
+
     
     
     app.get('/ui/style.css', function (req, res) {
