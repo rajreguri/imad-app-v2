2371a029d56ffaf91aa77c92535d3c7df0e3f556
rajreguri <rajreguri1@gmail.com>
2017-02-08 14:10:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'calender.html'));
 });
-
-
-    
     
     app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -20,6 +17,10 @@ app.get('/ui/main.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.jpg'));
 });
 
+
+app.get('/ui/A.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'A.html'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
