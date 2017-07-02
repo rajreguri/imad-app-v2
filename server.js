ed57a2f3f60615bbbc355568ead79fb09217a11f
rajreguri <rajreguri1@gmail.com>
2017-07-02 12:40:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/img493.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'img493.jpg'));
+app.get('/ui/img493.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'img493.png'));
 });
 
 
